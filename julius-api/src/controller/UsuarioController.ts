de1c3a73588974d1d6fae6552e9a54ee5a261f2c
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-14 11:08:48 -0300

    criando variavel para acessar o repositorio de usuarios e implementando o metodo de recuperar lançamentos por usuario

--- julius-api/src/controller/UsuarioController.ts
+++ julius-api/src/controller/UsuarioController.ts
@@ -1,21 +1,35 @@
 import { AppDataSource } from "../data-source"
 import { Usuario } from "../entity/Usuario";
 
-const { manager } = AppDataSource
-
 export class UsuarioController {
+    
+    private usuarioRepositorio = AppDataSource.getRepository(Usuario)
+
     async salvar(usuario: Usuario) {
-        const usuarioSalvo = await manager.save(usuario)
+        const usuarioSalvo = await this.usuarioRepositorio.save(usuario)
         return usuarioSalvo
     }
 
     async recuperarTodos() {
-        const usuarios = await manager.find(Usuario)
+        const usuarios = await this.usuarioRepositorio.find()
         return usuarios
     }
 
     async recuperarPorId(id: number) {
-        const usuario = await manager.findOneBy(Usuario, {id: id})
+        const usuario = await this.usuarioRepositorio.findOneBy({id: id})
         return usuario
+    }
+
+    async recuperarLancamentosDoUsuario(id: number) {
+        const usuario = await this.usuarioRepositorio.findOne({
+            where: {
+                id: id
+            },
+            relations: {
+                lancamentos: true
+            }
+        })
+        
+        return usuario.lancamentos
     }
 }
