c8464cb42d7a5a34555280a359d6b50782affe67
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-11-10 10:32:28 -0300

    testando com o sqlite

--- julius-api/src/data-source.ts
+++ julius-api/src/data-source.ts
@@ -1,15 +1,18 @@
+import { join } from "path"
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Lancamento } from "./entity/Lancamento"
 import { Usuario } from "./entity/Usuario"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "julius",
+    // type: "postgres",
+    // host: "localhost",
+    // port: 5432,
+    // username: "postgres",
+    // password: "postgres",
+    // database: "julius",
+    type: "sqlite",
+    database: join(__dirname, "bd_teste.db"),
     synchronize: true,
     logging: false,
     entities: [Usuario, Lancamento],
--- julius-api/src/routes/lancamento.ts
+++ julius-api/src/routes/lancamento.ts
@@ -9,8 +9,9 @@ const lancamentoCtrl = new LancamentoController()
 const usuarioCtrl = new UsuarioController()
 
 // Serviço para salvar um novo lançamento
-routerLancamento.post('/', async(req, res) => {
-    const { idUsuario, valor, descricao, data } = req.body
+routerLancamento.post('/:idUsuario', async(req, res) => {
+    const idUsuario = parseInt(req.params.idUsuario)
+    const { valor, descricao, data } = req.body
     const usuario = await usuarioCtrl.recuperarPorId(idUsuario)
     if (usuario) {
         const lancamento = new Lancamento(valor, descricao, data, usuario)
